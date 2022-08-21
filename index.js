ff7e4262cfa44962786c9fdabeb610148f2adea6
Jose Kupeka Muhlanga <jose.muhlanga@student.moringaschool.com>
2022-08-21 20:13:10 +0300

    Completed on Aug 21

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function superbowlWin(record){
+    let singledElement;
+    singledElement = record.find(el => el.result === "W");
+
+    if(singledElement){
+        return singledElement.year;
+    }else{
+        return undefined;
+    } 
+}
+
+
+
+
+
+
+
+
